e039a4365d3708392163b5e3d8781f8a935bc3fc
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-05 11:54:16 -0400

    add routes nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  resources :positions
-  resources :locations
-  resources :jobs
-  resources :skills
+  # resources :positions
+  # resources :locations
+  # resources :jobs
+  # resources :skills
   devise_for :users
+
+  namespace :api do
+      resources :positions
+      resources :locations do
+        resources :jobs
+      end
+      resources :skills
+  end
 
   root 'client#index'
   get '*path', to: 'client#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
