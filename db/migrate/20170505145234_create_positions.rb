1f5daa556e8f8656266fb7406b9d285b52546c8b
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-05 10:58:40 -0400

    add Position MVC

---
+++ db/migrate/20170505145234_create_positions.rb
@@ -0,0 +1,11 @@
+class CreatePositions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :positions do |t|
+      t.string :name
+      t.text :description
+      t.integer :qualification
+
+      t.timestamps
+    end
+  end
+end
