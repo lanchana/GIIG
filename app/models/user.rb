778096fdf034c0e45638d7267bb7814d0d1335de
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-05 12:01:10 -0400

    Include has_many and belongs to in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    has_many :skills, dependent: :destroy
+    has_many :jobs
+    # make sure this is working
+    has_many :jobs, through: :locations
+
+
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
