aeb7200886153810c297dc21a341f4b5b6fc8e27
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-07 12:21:40 -0400

    Add delete skill routes

--- app/assets/javascripts/components/skills/skillsIndex/skillsIndex.controller.js
+++ app/assets/javascripts/components/skills/skillsIndex/skillsIndex.controller.js
@@ -8,8 +8,24 @@ function SkillsIndexController(skillsService) {
   var vm = this;
 
   vm.skills = [];
+  vm.skillDelete = skillDelete;
 
+  activate();
+
+  function  activate() {
   skillsService.getSkills().then(function(response) {
     vm.skills = response.data;
   });
+}
+
+function skillDelete(skill_id) {
+    console.log("here in skill delete");
+    skillsService.deleteSkill(skill_id)
+                 .then((response) => {
+                    activate();
+                 })
+
+}
+
+
 }
