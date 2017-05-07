01bbc318fbea7554345f9ac51ec82bdae5b0e869
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-07 17:27:19 -0400

    maps updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,16 +31,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
 # enable send mail with specified account
-ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-  :user_name => 'lanchanagupta@gmail.com',
-  :password => 'lanch@vini<3',
-  :domain => 'gmail.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
 
   config.action_mailer.perform_caching = false
 
