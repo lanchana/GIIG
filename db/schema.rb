d02134632eeb960ac4754d30b492a7b0f3877f9f
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-07 18:03:09 -0400

    add photo url field to sign up and edit forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505182504) do
+ActiveRecord::Schema.define(version: 20170507220006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20170505182504) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "org"
+    t.string   "photo_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
