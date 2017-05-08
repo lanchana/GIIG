f24c8b6a45d569d03015eb9898987b731837b567
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-08 11:57:14 -0400

    ApllicationMailer::Base --> ApplicationMailer

--- app/mailers/giig_notifier_mailer.rb
+++ app/mailers/giig_notifier_mailer.rb
@@ -1,4 +1,4 @@
-class GiigNotifierMailer < ApplicationMailer::Base
+class GiigNotifierMailer < ApplicationMailer
     default :from => 'any_from_address@example.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
