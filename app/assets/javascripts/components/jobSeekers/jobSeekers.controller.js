105dacfe6e24fc84f36b5caa94d8036b434297e7
hernquist <hernquistdavid@gmail.com>
2017-05-09 10:46:21 -0400

    problem solved

--- app/assets/javascripts/components/jobSeekers/jobSeekers.controller.js
+++ app/assets/javascripts/components/jobSeekers/jobSeekers.controller.js
@@ -1,31 +1,32 @@
+Brandon King [10:38 AM] 
 angular
-  .module('GiiG')
-  .controller('JobseekersController', JobseekersController);
+  .module(‘GiiG’)
+  .controller(‘JobseekersController’, JobseekersController);
 
-JobseekersController.$inject = ['$stateParams', 'jobseekersService'];
+JobseekersController.$inject = [‘$stateParams’, ‘jobseekersService’];
 
 function JobseekersController($stateParams, jobseekersService) {
   var vm = this;
 
-  vm.jobs = [];
+ vm.jobs = [];
   vm.user_id = $stateParams.id;
   // vm.acceptJob  = acceptJob;
 
-  activate(); 
+ activate();
 
-  function activate() {
-    console.log ("jobseekers Contoller 10 :" + vm.user_id);
-  	jobseekersService.getJobs().then(function(response) {
-  	  vm.jobs = response.data;
+ function activate() {
+    console.log (“jobseekers Contoller 10 :” + vm.user_id);
+      jobseekersService.getJobs(vm.user_id).then(function(response) {
+        vm.jobs = response.data;
       // console.log(vm.jobs);
-  	});
+      });
   }
 
-  // function acceptJob(user_id, job_id) {
-  //   console.log("delete"+ job_id);
+ // function acceptJob(user_id, job_id) {
+  //   console.log(“delete”+ job_id);
   //   jobsService.deleteJob(location_id, job_id)
   //             .then((response) => {
-  //               console.log("delete job 2");
+  //               console.log(“delete job 2”);
   //               activate();
   //            });
   
