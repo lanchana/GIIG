9492e37a07c463055f056a315d3e42ab4d79aaaf
hernquist <hernquistdavid@gmail.com>
2017-05-09 12:24:53 -0400

    building the locations on the jsdashboard

--- app/assets/javascripts/components/jobSeekers/jobSeekers.controller.js
+++ app/assets/javascripts/components/jobSeekers/jobSeekers.controller.js
@@ -2,9 +2,11 @@ angular
   .module('GiiG')
   .controller('JobSeekersController', JobSeekersController);
 
-JobSeekersController.$inject = ['$stateParams', 'jobseekersService'];
+JobSeekersController.$inject = ['$stateParams', 
+                            'jobseekersService', 
+                            'locationsService']';
 
-function JobSeekersController($stateParams, jobseekersService) {
+function JobSeekersController($stateParams, jobseekersService, jobseekersService) {
   var vm = this;
 
  vm.jobs = [];
@@ -19,6 +21,17 @@ function JobSeekersController($stateParams, jobseekersService) {
         vm.jobs = response.data;
       // console.log(vm.jobs);
       });
+
+    function showLocation(location_id) {
+      locationsService.getLocation($stateParams.id).then(function(response) {
+        vm.location = response.data;
+        // console.log(vm.location);
+        }
+      }
+    });
+
+
+
   }
 
  // function acceptJob(user_id, job_id) {
