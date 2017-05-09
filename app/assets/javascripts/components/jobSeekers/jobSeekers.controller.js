590f1f890a602ad1614d71f1ec8d7566c1e9b1b7
hernquist <hernquistdavid@gmail.com>
2017-05-09 12:00:11 -0400

    stupid sublime glitches

--- app/assets/javascripts/components/jobSeekers/jobSeekers.controller.js
+++ app/assets/javascripts/components/jobSeekers/jobSeekers.controller.js
@@ -1,10 +1,10 @@
 angular
-  .module(‘GiiG’)
-  .controller(‘JobSeekersController’, JobSeekersController);
+  .module('GiiG')
+  .controller('JobSeekersController', JobSeekersController);
 
-JobSeekersController.$inject = [‘$stateParams’, ‘jobseekersService’];
+JobSeekersController.$inject = ['$stateParams', 'jobseekersService'];
 
-function JobseekersController($stateParams, jobseekersService) {
+function JobSeekersController($stateParams, jobseekersService) {
   var vm = this;
 
  vm.jobs = [];
@@ -14,7 +14,7 @@ function JobseekersController($stateParams, jobseekersService) {
  activate();
 
  function activate() {
-    console.log (“jobseekers Contoller 10 :” + vm.user_id);
+    console.log ('jobseekers Contoller 10 :' + vm.user_id);
       jobseekersService.getJobs(vm.user_id).then(function(response) {
         vm.jobs = response.data;
       // console.log(vm.jobs);
@@ -25,7 +25,7 @@ function JobseekersController($stateParams, jobseekersService) {
   //   console.log(“delete”+ job_id);
   //   jobsService.deleteJob(location_id, job_id)
   //             .then((response) => {
-  //               console.log(“delete job 2”);
+  //               console.log('delete job 2');
   //               activate();
   //            });
   
