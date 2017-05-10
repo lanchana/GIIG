dd1e71eacbcc2f26cecc35a6eedabea6839224a7
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-09 22:04:10 -0400

    add auth sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,19 @@ class User < ApplicationRecord
 
 
     devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.full_name = auth.info.name
+        user.email = auth.info.email
+        user.avatar = auth.info.image
+        user.password = Devise.friendly_token[0,20]
+      end
+  end
 
    has_attached_file :avatar
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509160738) do
+ActiveRecord::Schema.define(version: 20170510011938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 20170509160738) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
