a01c9c555195be4851f52151be2e17c52b5dbd4f
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-10 18:59:06 -0400

    Style up sign up form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:phone_num, :full_name, :address, :city, :state, :zipcode, :photo_url, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:org, :phone_num, :full_name, :address, :city, :state, :zipcode, :photo_url, :avatar])
   end
 
   # The path used after sign up.
